a5db93625199dc625d7b30e7bac9de0a12108e94
BezverhovDanil <danil.bezverhov272003@mail.ru>
2025-02-12 02:56:02 +0200

    fix

--- local/components/parse/parse.news/templates/.default/template.php
+++ local/components/parse/parse.news/templates/.default/template.php
@@ -2,8 +2,8 @@
     
     <div class="col-lg-4 col-sm-6 mb-4">
         <div class="portfolio-item">
-        <a class="portfolio-link text-decoration-none" href="/local/components/parse/parse.news/article.php?url=<?= urlencode($newsItem['DESCRIP']) ?>">
-        <div class="portfolio-hover">
+        <a class="portfolio-link text-decoration-none" href="/article<?= $newsItem['DESCRIP'] ?>">
+                    <div class="portfolio-hover">
                     <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                 </div>
                 <img class="img-fluid" src="<?= SITE_TEMPLATE_PATH ?>/assets/img/portfolio/<?= (($index % 6) + 1) ?>.jpg" alt="..." />
